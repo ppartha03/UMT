8b6cc958816de6d15b02289376010fc6425dd8d9
ppartha03 <prasannaparthasarathi@Prasannas-MacBook-Pro.local>
2021-03-05 11:12:24 -0500

    local changes

--- translate.py
+++ translate.py
@@ -12,11 +12,11 @@ import itertools
 import spacy
 import math
 import csv
-import json
 import wandb
 from itertools import product
 import sys
 from filelock import FileLock
+from transformers import MarianTokenizer, MarianMTModel
 
 import time
 
@@ -41,6 +41,7 @@ def HyperEvaluate(config):
     ext_language = config['lang']
     perturbation = config['perturb']
     model = config['model']
+    batch_size = config['batch_size']
 
     nlp_o = spacy.load(ext_language+"_core_news_sm")
     nlp = spacy.load("en_core_web_sm")
@@ -55,6 +56,9 @@ def HyperEvaluate(config):
 
     lines = test_file.readlines()
 
+    model = MarianMTModel.from_pretrained("/home/pparth2/scratch/UMT/UMT/Results/cached/Helsinki-NLP-opus-en-" + ext_language + "-model").cuda()
+    tokenizer = MarianTokenizer.from_pretrained("/home/pparth2/scratch/UMT/UMT/Results/cached/Helsinki-NLP-opus-en-" + ext_language).cuda()
+
     # todo : Save samples in a csv : with metrics, perturbed example and beams
     # todo : ensure the beams have the same seed across runs and so do the perturbation functions. Use the index as seed value.
 
@@ -62,61 +66,43 @@ def HyperEvaluate(config):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    eng_gold_file =  open(os.path.join(target_dir, 'en.gold'), "w")
-    eng_perturb_file = open(os.path.join(target_dir, 'en.perturb'), "w")
+    eng_gold_file =  open(os.path.join(target_dir, 'en.gold'), "r")
+    eng_perturb_file = open(os.path.join(target_dir, 'en.perturb'), "r")
 
-    o_lang_gold_file = open(os.path.join(target_dir, ext_language + '.gold'), "w")
-    o_lang_perturb_file = open(os.path.join(target_dir, ext_language + '.perturb'), "w")
+    o_lang_gold_file = open(os.path.join(target_dir, ext_language + '.gold.translate'), "w")
+    o_lang_perturb_file = open(os.path.join(target_dir, ext_language + '.perturb.translate'), "w")
 
-    lock = FileLock(os.path.join(target_dir, 'lock.l'))
+    eng_gold_sents = eng_gold_file.readlines()
+    eng_perturb_sents = eng_perturb_file.readlines()
 
-    counter_file = open(os.path.join('Data', model, ext_language,'stats.txt'),'a')
-
-    this_perturbation = total_sentences = len(lines) // 4
+    assert len(eng_gold_sents) == len(eng_perturb_sents)
 
     original_id = 0
     id_ = 0
 
     with lock:
-        for i in range(0,len(lines),4):
-            try:
-                other_lang_gold = lines[i].strip()
-                eng_gold = lines[i+1].strip()
+        for i in range(0,len(eng_gold_sents), batch_size):
+            e_gold_batch = [eval(_) for _ in eng_gold_sents[i: i+batch_size]]
+            e_perturb_batch = [eval(_) for _ in eng_perturb_sents[i: i+batch_size]]
 
-                eng_perturbed = perturbation(eng_gold, nlp = nlp)
-                other_lang_perturbed = perturbation(other_lang_gold, nlp = nlp_o)
+            eng_gold_batch = [_['text'] for _ in e_gold_batch]
+            eng_perturb_batch = [_['text'] for _ in e_perturb_batch]
 
-                d = {"original_id": original_id, "id", id_, "text": eng_gold}
-                d_f = json.dumps(d)
-                eng_gold_file.write(d_f + '\n')
+            batch_gold = tok.prepare_seq2seq_batch(src_texts=eng_gold_batch, return_tensors="pt")
+            batch_perturb = tok.prepare_seq2seq_batch(src_texts=eng_perturb_batch, return_tensors="pt")
 
-                d = {"original_id": original_id, "id", id_, "text": other_lang_gold}
-                d_f = json.dumps(d)
-                o_lang_gold_file.write(d_f + '\n')
+            gen = model.generate(**batch_gold)
+            translate_gold = tok.batch_decode(gen, skip_special_tokens=True)
 
-                d = {"original_id": original_id, "id", id_, "text": eng_perturbed}
-                d_f = json.dumps(d)
-                eng_perturb_file.write(d_f + '\n')
+            gen = model.generate(**batch_perturb)
+            translate_perturb = tok.batch_decode(gen, skip_special_tokens=True)
 
-                d = {"original_id": original_id, "id", id_, "text": other_lang_perturbed}
-                d_f = json.dumps(d)
-                o_lang_perturb_file.write(d_f + '\n')
+            for k in e_gold_batch:
+                original_id = e_gold_batch[k]['original_id']
+                id_ = e_gold_batch[k]['id']
 
-                id_ += 1
-            except AssertionError:
-                this_perturbation -= 1
-                pass
-            except:
-                this_perturbation -= 1
-                pass
-            original_id += 1
-
-    counter_lock = FileLock(os.path.join('Data', model, ext_language, 'counter_lock'))
-
-    with counter_lock:
-        counter_file = open(os.path.join('Data', model, ext_language,'stats.txt'),'a')
-        counter_file.write(perturbation.__name__ + ' : ' + str(this_perturbation) + '/' + str(total_sentences) + '\n')
-    counter_file.close()
+                o_lang_perturb_file.write('{ ' + '\"original_id\": ' + str(original_id) + ', \"id\": ' + str(id_) + ',\"text\": \"' + translate_perturb[k] + '\"}\n')
+                o_lang_gold_file.write('{ ' + '\"original_id\": ' + str(original_id) + ', \"id\": ' + str(id_) + ',\"text\": \"' + translate_gold[k] + '\"}\n')
 
     o_lang_gold_file.close()
     eng_gold_file.close()
@@ -124,17 +110,15 @@ def HyperEvaluate(config):
     eng_perturb_file.close()
     o_lang_perturb_file.close()
 
-    counter_file.close()
-
 
 if __name__ == '__main__':
 
     PARAM_GRID = list(product(
     ['Helsinki-opus'], #model
-    ['de','fr','ru','ja'], #languages#[verbAtBeginning]
+    ['de','fr','ru','ja'], #languages
     [treeMirrorPre, treeMirrorPo, treeMirrorIn, verbSwaps, adverbVerbSwap, verbAtBeginning,
       nounVerbSwap, nounVerbMismatched, nounAdjSwap, shuffleHalvesFirst, shuffleHalvesLast,
-      reversed, wordShuffle, rotateAroundRoot,functionalShuffle, nounSwaps, conjunctionShuffle],
+      reversed, wordShuffle, rotateAroundRoot,functionalShuffle, nounSwaps, conjunctionShuffle]
     )
     )
 
@@ -167,12 +151,12 @@ if __name__ == '__main__':
     workers_per_gpu = 10
     executor = submitit.AutoExecutor(folder=submitit_logdir)
     executor.update_parameters(
-        timeout_min=30,
+        timeout_min=120,
         gpus_per_node=num_gpus,
         slurm_additional_parameters={"account": "rrg-bengioy-ad"},
         tasks_per_node=num_gpus,
         cpus_per_task=workers_per_gpu,
-        slurm_mem="8G",#16G
+        slurm_mem="16G",#16G
         slurm_array_parallelism=100,
     )
     job = executor.map_array(HyperEvaluate,h_param_list)
